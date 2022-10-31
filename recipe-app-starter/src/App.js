45dcd3be133c88298fd8affd0fae6e838b7ed362
Yasin Yavuz <barbaros1675@gmail.com>
2022-10-31 21:04:13 +0100

    solution %50 finished

--- recipe-app-starter/src/App.js
+++ recipe-app-starter/src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
+import "./App.css";
+import AppRouter from "./router/AppRouter";
+import { ThemeProvider } from "styled-components";
+import theme from "./components/globalStyles/theme";
 
 function App() {
   return (
-    <div className="App">
-     RECIPE APP
-    </div>
+    <ThemeProvider theme={theme}>
+      <AppRouter />
+    </ThemeProvider>
   );
 }
 
 export default App;
-
