b44c60bd0e1b6fddf33b7bbfb0235ab1060d05b1
Yasin Yavuz <barbaros1675@gmail.com>
2022-12-24 12:54:00 +0100

    style components added

--- recipe-app-starter/src/App.js
+++ recipe-app-starter/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import AppRouter from "./router/AppRouter";
 
 function App() {
-  return <div classname="App">RECIPE APP</div>;
+  return (
+    <div classname="App">
+      <AppRouter />
+    </div>
+  );
 }
 
 export default App;
--- recipe-app-starter/src/components/nav/Navbar.jsx
+++ recipe-app-starter/src/components/nav/Navbar.jsx
@@ -1,29 +1,7 @@
-import React,{useState} from "react";
-import "./Navbar.style";
-import Nav, { Brand ,Menu,MenuLink,Hamburger} from "./Navbar.style";
-import {GiHamburgerMenu} from "react-icons/gi"
+import React from "react";
 
 const Navbar = () => {
-  const [isOpen,setIsOpen]=useState(false)
-  return (
-    <Nav justify="space-between" wrap="wrap">
-      <Brand to="/">
-        <i>{"<Clarusway/>"}</i>
-        <span>Recipe</span>
-      </Brand>
-
-      <Hamburger onClick={()=>setIsOpen(!isOpen)}>
-        <GiHamburgerMenu/>
-      </Hamburger>
-
-      <Menu isOpen={isOpen} onClick={()=>setIsOpen(false)}>
-        <MenuLink to="/">Home</MenuLink>
-        <MenuLink to="about">About</MenuLink>
-        <MenuLink to="register">Register</MenuLink>
-        <MenuLink to="Logout" onClick={()=>sessionStorage.clear()}>Logout</MenuLink>
-      </Menu>
-    </Nav>
-  );
+  return <div>Navbar</div>;
 };
 
 export default Navbar;
