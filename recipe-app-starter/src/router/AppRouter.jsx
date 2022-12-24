7a84218d1fe89f286a001d7473d4078232225309
Yasin Yavuz <barbaros1675@gmail.com>
2022-12-24 13:31:53 +0100

    global added

--- recipe-app-starter/src/router/AppRouter.jsx
+++ recipe-app-starter/src/router/AppRouter.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./../components/nav/Navbar";
+import { GlobalStyles } from "../components/globalStyles/GlobalStyles";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
+      <GlobalStyles />
       <Navbar />
       <Routes>
         <Route />
