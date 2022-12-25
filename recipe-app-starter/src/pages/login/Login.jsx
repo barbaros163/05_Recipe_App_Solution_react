573a956b8922bc4a0d7d6420e7ed920f177d0715
Yasin Yavuz <barbaros1675@gmail.com>
2022-12-25 13:48:55 +0100

    login added

--- recipe-app-starter/src/pages/login/Login.jsx
+++ recipe-app-starter/src/pages/login/Login.jsx
@@ -1,9 +1,42 @@
 import React from "react";
-
-import LoginContainer from "./Login.style";
+import {
+  LoginContainer,
+  FormContainer,
+  StyledImg,
+  Header,
+  StyledInput,
+  StyledForm,
+  StyledButton,
+} from "./Login.style";
+import { useNavigate } from "react-router-dom";
+import meal from "../../assets/meal.svg";
 
 const Login = () => {
-  return <LoginContainer></LoginContainer>;
+  const navigate = useNavigate();
+
+  const userInfo = {
+    username: "admin",
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sessionStorage.setItem("user", JSON.stringify(userInfo));
+    navigate(-1);
+  };
+
+  return (
+    <LoginContainer>
+      <FormContainer>
+        <StyledImg src={meal} />
+        <Header>{"<CW/>"}Recipe</Header>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput placeholder="Enter username" type="text" />
+          <StyledInput placeholder="Enter Password" type="password" />
+          <StyledButton type="submit">Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
+  );
 };
 
 export default Login;
