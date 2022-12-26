a1d86c540e5f907b93d0ebfa0747254a2253a558
Yasin Yavuz <barbaros1675@gmail.com>
2022-12-26 13:10:41 +0100

    hamburger added

--- recipe-app-starter/src/components/nav/Navbar.jsx
+++ recipe-app-starter/src/components/nav/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Brand, Nav } from "./Navbar.style";
+import React, { useState } from "react";
+import "./Navbar.style";
+import Nav, { Brand, Menu, MenuLink, Hamburger } from "./Navbar.style";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <Nav justify="space-between" wrap="wrap">
       <Brand to="/">
@@ -10,12 +12,18 @@ const Navbar = () => {
         <span>Recipe</span>
       </Brand>
 
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="about">About</Link>
-        <Link to="register">Register</Link>
-        <Link to="logout">Logout</Link>
-      </div>
+      <Hamburger onClick={() => setIsOpen(!isOpen)}>
+        <GiHamburgerMenu />
+      </Hamburger>
+
+      <Menu isOpen={isOpen} onClick={() => setIsOpen(false)}>
+        <MenuLink to="/">Home</MenuLink>
+        <MenuLink to="about">About</MenuLink>
+        <MenuLink to="register">Register</MenuLink>
+        <MenuLink to="Logout" onClick={() => sessionStorage.clear()}>
+          Logout
+        </MenuLink>
+      </Menu>
     </Nav>
   );
 };
