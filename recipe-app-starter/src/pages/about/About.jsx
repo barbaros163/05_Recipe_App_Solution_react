05186fbf1c36410547c251a9ae95c5593fd8b31d
Yasin Yavuz <barbaros1675@gmail.com>
2023-01-07 17:28:42 +0100

    some codes added

--- recipe-app-starter/src/pages/about/About.jsx
+++ recipe-app-starter/src/pages/about/About.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import coding from "../../assets/coding.svg";
+import { AboutContainer } from "./About.style";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <AboutContainer>
+      <img src={coding} alt={coding} />
+      <h1>About Software Developer Yasin Yavuz</h1>
+      <div className="p1">
+        <p>I'm Yasin</p>
+        <p>I'm currently learning Front End Developemnt Languages.</p>
+        <p>I've already known JS, ReactJS, ReactNative, NodeJS.</p>
+        <p>Send email: barbaros1675@gmail.com</p>
+      </div>
+    </AboutContainer>
+  );
 };
 
 export default About;
---
+++ recipe-app-starter/src/pages/about/About.style.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import Flex from "../../components/globalStyles/Flex.style";
+
+export const AboutContainer = styled(Flex)`
+  display: flex;
+  flex-direction: column;
+`;
